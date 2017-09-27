08f0d42d39235c0a368d6c8ebecdb8e3f8e27b82
Salavat <salavat@macbook-air-sal.lan>
2017-09-28 01:10:57 +0300

    Initial Commit

---
+++ LinearSystem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinearSystem
+//
+//  Created by Salavat on 28/09/2017.
+//  Copyright © 2017 Salavat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
